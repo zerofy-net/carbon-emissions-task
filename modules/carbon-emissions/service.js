74555a357edeb63dd95687234bfeda1c2875a0b1
nkajl <nkajalin9@gmail.com>
2024-08-24 11:06:03 +0300

    added fetching data from electricity maps api

--- modules/carbon-emissions/service.js
+++ modules/carbon-emissions/service.js
@@ -1,13 +1,47 @@
+const axios = require("axios");
+
 function calculateCarbonEmissions(consumption, intensity) {
 	return emissions;
 }
 
 async function fetchPowerConsumption(){
-	return powerConsumption;
+	try {
+		const powerBreakdownResponse = await axios.get(process.env.API_URL + 'v3/power-breakdown/history', {
+			headers: {'auth_token' : `${process.env.API_TOKEN}`}
+		})
+		if(powerBreakdownResponse.status === 200){
+			const powerConsumption = powerBreakdownResponse.map(elem => ({
+				datetime: elem.datetime,
+				powerConsumptionTotal: elem.powerConsumptionTotal
+			}));
+
+			return powerConsumption;
+		} else {
+			logger.error('Unsuccessful power breakdown response', {status: powerBreakdownResponse.status});
+		}
+	} catch (error) {
+		throw new Error(`Error fetching power breakdown response: ${error.message}`);
+	}
 }
 
 async function fetchCarbonIntensity(){
-	return carbonIntensity;
+	try {
+		carbonIntensityResponse = await axios.get(process.env.API_URL + 'v3/carbon-intensity/history', {
+			headers: {'auth_token' : `${process.env.API_TOKEN}`}
+		})
+		if(carbonIntensityResponse.status === 200){
+			const carbonIntensity = powerBreakdownResponse.map(elem => ({
+				datetime: elem.datetime,
+				carbonIntensity: elem.carbonIntensity
+			}));
+
+			return carbonIntensity;
+		} else {
+			logger.error('Unsuccessful carbon intensity response', {status: carbonIntensityResponse.status});
+		}
+	} catch (error) {
+		throw new Error(`Error fetching carbon intensity response: ${error.message}`);
+	}
 }
 
 module.exports = { calculateCarbonEmissions, fetchCarbonIntensity, fetchPowerConsumption };
